4c4be4d2a4ff0afcaf75ac66cb753f18e3a41035
Dara Al Ali <dara008@hotmail.com>
2018-11-16 18:46:22 +0100

    'lll'

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,14 +10,32 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array
+    //  * @var array
      */
-    protected $proxies;
+    // protected $proxies;
 
+    protected $proxies = [
+        '192.168.1.1',
+        '192.168.1.2',
+
+    ];
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    //  * @var int
+    //  */
+    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
+
+
+
+
 }
